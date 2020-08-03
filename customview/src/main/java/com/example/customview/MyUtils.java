1061eb68540d72b32156fdecab943185f086f9ec
chason <754702559@qq.com>
2020-08-03 13:52:21 +0800

    first commit

---
+++ customview/src/main/java/com/example/customview/MyUtils.java
@@ -0,0 +1,16 @@
+package com.example.customview;
+
+import android.app.Application;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * @Author: chason
+ * @Description:
+ * @CreateDate: 2020/8/3 1:48 PM
+ */
+public class MyUtils {
+    public void TestUtils() {
+        Log.i("chason", "TestUtils");
+    }
+}
